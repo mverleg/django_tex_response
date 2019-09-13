215d0b9e28d39d8801d2907bac02313ef33534de
Mark <mverleg.noreply@gmail.com>
2019-09-13 22:14:29 +0200

    Attempt to store latex as temp file

--- docker_service/source/gen/latex.py
+++ docker_service/source/gen/latex.py
@@ -1,11 +1,14 @@
 from os import remove
+from tempfile import NamedTemporaryFile
 
 from tex_response.tex import render_tex, tex_to_pdf
 
 
-def render_pdf_binary(template, context):
-    tex_file = render_tex(None, template, context)
-    pdf_tmp = tex_to_pdf(tex_file, tex_cmd='lualatex',
+def render_pdf_binary(latex_bin, context):
+    #tex_file = render_tex(None, template, context)
+    latex_file = NamedTemporaryFile()
+    latex_file.write(latex_bin)
+    pdf_tmp = tex_to_pdf(latex_file, tex_cmd='lualatex',
         flags=('-interaction=nonstopmode', '-halt-on-error',),
         do_link_imgs=True)
     with open(pdf_tmp, 'rb') as fh:
