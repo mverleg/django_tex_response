1cecb94a30fdc464ad4e2f4310001d8d850fd6fd
Mark <mverleg.noreply@gmail.com>
2019-09-13 08:07:33 +0200

    Read file content, and better for muse

--- docker_service/source/gen/api.py
+++ docker_service/source/gen/api.py
@@ -1,10 +1,11 @@
 import time
 
 from django.conf import settings
-from django.http import HttpResponseForbidden, HttpResponseBadRequest, HttpResponse
+from django.http import HttpResponseBadRequest, HttpResponse
 
 from docker_service.source.gen.form import LatexToPdfRequest
 from docker_service.source.gen.latex import render_pdf_binary
+from tex_response import LatexException
 
 
 class Counter:
@@ -25,21 +26,21 @@ def generate_latex_file(request):
         return HttpResponse("Send a post request containing 'key' and 'latex'\n", status=405)
     if not request.FILES:
         return HttpResponse("use 'enctype=\"multipart/form-data\"'\n", status=405)
-
-    forms = LatexToPdfRequest(request.POST, request.FILES)
-    if not forms.is_valid():
-        pass
-    #TODO @mark: 
-
-    if 'key' not in request.POST:
-        return HttpResponseForbidden("request should contain 'key' as post data\n")
-    key = request.POST.get('key', None)
+    form = LatexToPdfRequest(request.POST, request.FILES)
+    if not form.is_valid():
+        return HttpResponseBadRequest('{}\n{}\n'.format('\n'.join(form.errors.values()), form.non_field_errors()))
+    key = form.cleaned_data['key']
     if settings.ACCESS_KEY != key:
-        return HttpResponseForbidden("the provided post 'key' was incorrect\n")
-    if not request.FILES or 'latex' not in request.FILES:
-        return HttpResponseBadRequest("request should contain 'latex' as post data\n")
-    latex = request.FILES.get('latex', None)
-    pdf = render_pdf_binary(latex, {})
+        return HttpResponse("the provided post 'key' was incorrect\n", status=401)
+    # if not request.FILES or 'latex' not in request.FILES:
+    #     return HttpResponseBadRequest("request should contain 'latex' as post data\n")
+    latex_file = form.cleaned_data['latex']
+    latex = latex_file.file.read()
+    try:
+        #TODO @mark: this still does a djanog render, but I have no context - rather just render directly
+        pdf = render_pdf_binary(latex, {})
+    except LatexException as ex:
+        return HttpResponseBadRequest(ex.message)
     name = '{}_{}.pdf'.format(time.time(), Counter.next())
     response = HttpResponse(pdf, content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="\'{}\'"'.format(name)
