929c46a9afd82c6b1f6b22646cafa6d5be2ff2af
Mark <mverleg.noreply@gmail.com>
2019-09-15 22:09:07 +0200

    Attempt to make alpine py3 find my package, to no avail

--- setup.py
+++ setup.py
@@ -5,17 +5,30 @@ For installing with pip
 """
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='tex_response',
-    version='1.2.2',
+    version='1.2.1',
     author=u'Mark V',
     author_email='markv.nl.dev@gmail.com',
     packages=['tex_response'],
     include_package_data=True,
-    url='https://bitbucket.org/mverleg/django_tex_response',
+    url='https://github.com/mverleg/django_tex_response',
     license='revised BSD license; see LICENSE.txt',
-    description='Simple Django code that lets you use your installed TeX compiler to render a .tex template to a pdf-file response.',
+    description='Simple Django code that lets you use your installed luatex compiler to render a .tex template to a pdf-file response.',
     zip_safe=True,
-    install_requires = [],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        # 'Topic :: Utilities',
+    ],
+
+    install_requires=[],
 )
